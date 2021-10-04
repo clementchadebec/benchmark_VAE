c81264cb3e80ee0236ae41198bb28d7b26afb029
clementchadebec <clement.chadebec@orange.fr>
2021-10-04 16:24:54 +0200

    update vamp config

--- src/pythae/models/vamp/vamp_config.py
+++ src/pythae/models/vamp/vamp_config.py
@@ -1,11 +1,11 @@
 from pydantic.dataclasses import dataclass
 
-from ..base.base_config import BaseAEConfig
+from ...models import VAEConfig
 
 @dataclass
-class VAMPConfig(BaseAEConfig):
+class VAMPConfig(VAEConfig):
     """This is the variational autoencoder with Variational Mixture of Posterior (VAMP) model 
-    configuration instance deriving from:class:`~pythae.config.BaseAEConfig`.
+    configuration instance deriving from:class:`VAEConfig`.
 
     Parameters:
         input_dim (int): The input_data dimension
@@ -15,6 +15,4 @@ class VAMPConfig(BaseAEConfig):
         reconstruction_loss (str): The reconstruction loss to use ['bce', 'mse']. Default: 'mse'
         number_components (int): The number of components to use in the VAMP prior. Default: 50
         """
-
-    reconstruction_loss: str = 'mse'
     number_components: int = 50
