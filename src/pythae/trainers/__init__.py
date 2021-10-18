904c0a819e36903f1cb06939dc47202151ef873a
clementchadebec <clement.chadebec@orange.fr>
2021-10-18 19:44:11 +0200

    add dual optimizer trainer

--- src/pythae/trainers/__init__.py
+++ src/pythae/trainers/__init__.py
@@ -1,3 +1,4 @@
 from .base_trainer import BaseTrainer, BaseTrainingConfig
+from .coupled_optimizer_trainer import CoupledOptimizerTrainer, CoupledOptimizerTrainerConfig
 
-__all__ = ["BaseTrainer", "BaseTrainingConfig"]
+__all__ = ["BaseTrainer", "BaseTrainingConfig", "CoupledOptimizerTrainer", "CoupledOptimizerTrainerConfig"]
