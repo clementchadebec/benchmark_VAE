086228dc1dce6dee6f3d42f0a1d618b7df5eb61b
clementchadebec <clement.chadebec@orange.fr>
2021-10-18 11:24:07 +0200

    fix typo

--- src/pythae/models/rhvae/rhvae_model.py
+++ src/pythae/models/rhvae/rhvae_model.py
@@ -549,7 +549,7 @@ class RHVAE(VAE):
 
         if self.model_config.reconstruction_loss == 'mse':
 
-            recon_loss =  F.mse_loss(
+            recon_loss =  -F.mse_loss(
                 recon_x.reshape(x.shape[0], -1), x.reshape(x.shape[0], -1), reduction='none'
             ).sum()
 
