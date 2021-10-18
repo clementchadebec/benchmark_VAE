aea24e4396a4ac1a7fddc8d6763c94eb9d1a85a9
clementchadebec <clement.chadebec@orange.fr>
2021-10-18 12:30:44 +0200

    set torch version to 1.9.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     packages=find_packages(where="src"),
     install_requires=[
         "numpy>=1.19.0",
-        "torch>=1.9.0",
+        "torch==1.9.0",
         "dill>=0.3.3",
         "pydantic>=1.8.2",
         "dataclasses>=0.6",
