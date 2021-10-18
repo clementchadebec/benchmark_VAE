8c11ce06441487db5cb27dcf4eef9305c5e43a51
clementchadebec <clement.chadebec@orange.fr>
2021-10-18 09:24:38 +0200

    change demo training config in examples

--- src/pythae/trainers/base_trainer/base_training_config.py
+++ src/pythae/trainers/base_trainer/base_training_config.py
@@ -18,17 +18,18 @@ class BaseTrainingConfig(BaseConfig):
         output_dir (str): The directory where model checkpoints, configs and final
             model will be stored. Default: None.
 
-        batch_size (int): The number of training samples per batch. Default 50
-        num_epochs (int): The maximal number of epochs for training. Default: 10000
-        learning_rate (int): The learning rate applied to the `Optimizer`. Default: 1e-3
-        steps_saving (int): A model checkpoint will be saved every `steps_saving` epoch
+        batch_size (int): The number of training samples per batch. Default 100
+        num_epochs (int): The maximal number of epochs for training. Default: 100
+        learning_rate (int): The learning rate applied to the `Optimizer`. Default: 1e-4
+        steps_saving (int): A model checkpoint will be saved every `steps_saving` epoch. 
+            Default: None
         keep_best_on_train (bool): Whether to keep the best model on the train set. Default: False.
-        seed (int): The random seed for reprodicibility
+        seed (int): The random seed for reproducibility
         no_cuda (bool): Disable `cuda` training. Default: False
     """
 
     output_dir: str = None
-    batch_size: int = 50
+    batch_size: int = 100
     num_epochs: int = 100
     learning_rate: float = 1e-4
     steps_saving: Union[int, None] = None
