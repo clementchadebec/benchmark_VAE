c523863df8ee5727ec090b2786b1cd1ce83274c9
clementchadebec <clement.chadebec@orange.fr>
2021-10-26 10:24:28 +0200

    update config

--- examples/scripts/models_to_train.sh
+++ examples/scripts/models_to_train.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-python training.py --dataset celeba --model_name ae --model_config 'configs/celeba/ae_config.json' --training_config 'configs/celeba/base_training_config.json'
-python training.py --dataset celeba --model_name vae --model_config 'configs/celeba/vae_config.json' --training_config 'configs/celeba/base_training_config.json'
-python training.py --dataset celeba --model_name wae --model_config 'configs/celeba/wae_config.json' --training_config 'configs/celeba/base_training_config.json'
-#python training.py --dataset celeba --model_name vamp --model_config 'configs/celeba/vamp_config.json' --training_config 'configs/celeba/base_training_config.json'
-python training.py --dataset celeba --model_name rae_l2 --model_config 'configs/celeba/rae_l2_config.json' --training_config 'configs/celeba/base_training_config.json'
-python training.py --dataset celeba --model_name rae_gp --model_config 'configs/celeba/rae_gp_config.json' --training_config 'configs/celeba/base_training_config.json'
-python training.py --dataset celeba --model_name hvae --model_config 'configs/celeba/hvae_config.json' --training_config 'configs/celeba/base_training_config.json'
-python training.py --dataset celeba --model_name rhvae --model_config 'configs/celeba/rhvae_config.json' --training_config 'configs/celeba/base_training_config.json'
+#python training.py --dataset mnist --model_name vamp --model_config 'configs/mnist/vamp_config.json' --training_config 'configs/mnist/base_training_config.json'
+#python training.py --dataset mnist --model_name ae --model_config 'configs/mnist/ae_config.json' --training_config 'configs/mnist/base_training_config.json'
+#python training.py --dataset mnist --model_name vae --model_config 'configs/mnist/vae_config.json' --training_config 'configs/mnist/base_training_config.json'
+#python training.py --dataset mnist --model_name wae --model_config 'configs/mnist/wae_config.json' --training_config 'configs/mnist/base_training_config.json'
+#python training.py --dataset mnist --model_name rae_l2 --model_config 'configs/mnist/rae_l2_config.json' --training_config 'configs/mnist/base_training_config.json'
+#python training.py --dataset mnist --model_name rae_gp --model_config 'configs/mnist/rae_gp_config.json' --training_config 'configs/mnist/base_training_config.json'
+#python training.py --dataset mnist --model_name iwae --model_config 'configs/mnist/iwae_config.json' --training_config 'configs/mnist/base_training_config.json'
+python training.py --dataset mnist --model_name hvae --model_config 'configs/mnist/hvae_config.json' --training_config 'configs/mnist/base_training_config.json'
+python training.py --dataset mnist --model_name rhvae --model_config 'configs/mnist/rhvae_config.json' --training_config 'configs/mnist/base_training_config.json'
+
+
+
+#python training.py --dataset celeba --model_name ae --model_config 'configs/celeba/ae_config.json' --training_config 'configs/celeba/base_training_config.json'
+#python training.py --dataset celeba --model_name vae --model_config 'configs/celeba/vae_config.json' --training_config 'configs/celeba/base_training_config.json'
+#python training.py --dataset celeba --model_name wae --model_config 'configs/celeba/wae_config.json' --training_config 'configs/celeba/base_training_config.json'
+##python training.py --dataset celeba --model_name vamp --model_config 'configs/celeba/vamp_config.json' --training_config 'configs/celeba/base_training_config.json'
+#python training.py --dataset celeba --model_name rae_l2 --model_config 'configs/celeba/rae_l2_config.json' --training_config 'configs/celeba/base_training_config.json'
+#python training.py --dataset celeba --model_name rae_gp --model_config 'configs/celeba/rae_gp_config.json' --training_config 'configs/celeba/base_training_config.json'
+#python training.py --dataset celeba --model_name hvae --model_config 'configs/celeba/hvae_config.json' --training_config 'configs/celeba/base_training_config.json'
+#python training.py --dataset celeba --model_name rhvae --model_config 'configs/celeba/rhvae_config.json' --training_config 'configs/celeba/base_training_config.json'
