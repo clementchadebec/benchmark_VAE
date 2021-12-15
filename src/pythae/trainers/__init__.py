d909d70a8d142bb792f78098956a5aefeef4d190
clementchadebec <clement.chadebec@orange.fr>
2021-12-15 15:48:09 +0100

    add Adversarial trainer

--- src/pythae/trainers/__init__.py
+++ src/pythae/trainers/__init__.py
@@ -3,5 +3,13 @@
 
 from .base_trainer import BaseTrainer, BaseTrainingConfig
 from .coupled_optimizer_trainer import CoupledOptimizerTrainer, CoupledOptimizerTrainerConfig
+from .adversarial_trainer import AdversarialTrainer, AdversarialTrainerConfig
 
-__all__ = ["BaseTrainer", "BaseTrainingConfig", "CoupledOptimizerTrainer", "CoupledOptimizerTrainerConfig"]
+__all__ = [
+    "BaseTrainer",
+    "BaseTrainingConfig",
+    "CoupledOptimizerTrainer",
+    "CoupledOptimizerTrainerConfig",
+    "AdversarialTrainer",
+    "AdversarialTrainerConfig"
+]
---
+++ src/pythae/trainers/adversarial_trainer/adversarial_trainer.py
@@ -0,0 +1,184 @@
+import datetime
+import logging
+import os
+from copy import deepcopy
+from typing import Any, Dict, Optional
+from tqdm import tqdm
+import itertools
+
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from ...customexception import ModelError
+from ...data.datasets import BaseDataset
+from ...models import BaseAE
+from ..trainer_utils import set_seed
+from ..base_trainer import BaseTrainer
+from .adversarial_trainer_config import AdversarialTrainerConfig
+
+logger = logging.getLogger(__name__)
+
+# make it print to the console.
+console = logging.StreamHandler()
+logger.addHandler(console)
+logger.setLevel(logging.INFO)
+
+
+class AdversarialTrainer(BaseTrainer):
+    """Trainer using disctinct optimizers for encoder and decoder nn.
+
+    Args:
+        model (BaseAE): The model to train
+
+        train_dataset (BaseDataset): The training dataset of type 
+            :class:`~pythae.data.dataset.BaseDataset`
+
+        training_args (AdversarialTrainerConfig): The training arguments summarizing the main 
+            parameters used for training. If None, a basic training instance of 
+            :class:`AdversarialTrainerConfig` is used. Default: None.
+
+        encoder_decoder_optimizer (~torch.optim.Optimizer): An instance of `torch.optim.Optimizer` 
+            used for training the autoencoder. If None, a :class:`~torch.optim.Adam` optimizer is 
+            used. Default: None.
+
+        discriminator_optimizer (~torch.optim.Optimizer): An instance of `torch.optim.Optimizer` 
+            used for training the discriminator. If None, a :class:`~torch.optim.Adam` optimizer is 
+            used. Default: None.
+    """
+
+    def __init__(
+        self,
+        model: BaseAE,
+        train_dataset: BaseDataset,
+        eval_dataset: Optional[BaseDataset] = None,
+        training_config: Optional[AdversarialTrainerConfig] = None,
+        encoder_decoder_optimizer: Optional[torch.optim.Optimizer] = None,
+        discriminator_optimizer: Optional[torch.optim.Optimizer] = None
+    ):
+
+        BaseTrainer.__init__(
+            self,
+            model=model,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            training_config=training_config,
+            optimizer=None)
+
+        # set encoder optimizer
+        if encoder_decoder_optimizer is None:
+            encoder_decoder_optimizer = self.set_default_encoder_decoder_optimizer(model)
+
+        else:
+            encoder_decoder_optimizer = self._set_optimizer_on_device(
+                encoder_decoder_optimizer, self.device
+            )
+
+        # set decoder optimizer
+        if discriminator_optimizer is None:
+            discriminator_optimizer = self.set_default_discriminator_optimizer(model)
+
+        else:
+            discriminator_optimizer = self._set_optimizer_on_device(
+                discriminator_optimizer, self.device
+            )
+
+        self.encoder_decoder_optimizer = encoder_decoder_optimizer
+        self.discriminator_optimizer = discriminator_optimizer
+
+        self.optimizer = None
+
+
+    def set_default_encoder_decoder_optimizer(self, model: BaseAE) -> torch.optim.Optimizer:
+
+        optimizer = torch.optim.Adam(
+            itertools.chain(model.encoder.parameters(), model.decoder.parameters()),
+            lr=self.training_config.learning_rate,
+            weight_decay=self.training_config.encoder_decoder_optim_decay
+            )
+
+        return optimizer
+
+    def set_default_discriminator_optimizer(self, model: BaseAE) -> torch.optim.Optimizer:
+
+        optimizer = optim.Adam(
+            model.discriminator.parameters(),
+            lr=self.training_config.learning_rate,
+            weight_decay=self.training_config.discriminator_optim_decay
+        )
+
+        return optimizer
+
+    def train_step(self, epoch: int):
+        """The trainer performs training loop over the train_loader.
+
+        Parameters:
+            epoch (int): The current epoch number
+
+        Returns:
+            (torch.Tensor): The step training loss
+        """
+        # set model in train model
+        self.model.train()
+
+        epoch_loss = 0
+
+        with tqdm(self.train_loader, unit="batch") as tepoch:
+            for inputs in tepoch:
+
+                tepoch.set_description(
+                    f"Training of epoch {epoch}/{self.training_config.num_epochs}"
+                )
+
+                inputs = self._set_inputs_to_device(inputs)
+                model_output = self.model(inputs)
+
+                self.encoder_decoder_optimizer.zero_grad()
+                generator_loss = model_output.generator_loss
+                generator_loss.backward()
+                self.encoder_decoder_optimizer.step()
+
+                self.discriminator_optimizer.zero_grad()
+                discriminator_loss = model_output.discriminator_loss
+                discriminator_loss.backward()
+                self.discriminator_optimizer.step()
+
+                loss = generator_loss + discriminator_loss
+
+                epoch_loss += loss.item()
+
+            # Allows model updates if needed
+            self.model.update()
+
+            epoch_loss /= len(self.train_loader)
+
+        return epoch_loss
+
+
+    def save_checkpoint(self, dir_path, epoch: int):
+        """Saves a checkpoint alowing to restart training from here
+
+        Args:
+            dir_path (str): The folder where the checkpoint should be saved
+            epochs_signature (int): The epoch number"""
+
+        checkpoint_dir = os.path.join(dir_path, f"checkpoint_epoch_{epoch}")
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        # save optimizers
+        torch.save(
+            deepcopy(self.encoder_decoder_optimizer.state_dict()),
+            os.path.join(checkpoint_dir, "encoder_decoder_optimizer.pt"),
+        )
+        torch.save(
+            deepcopy(self.discriminator_optimizer.state_dict()),
+            os.path.join(checkpoint_dir, "discriminator_optimizer.pt"),
+        )
+
+        # save model
+        self.model.save(checkpoint_dir)
+
+        # save training config
+        self.training_config.save_json(checkpoint_dir, "training_config")
