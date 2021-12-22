ef4cc4a955eb9a389d97ea33fe10030fcf6698df
clementchadebec <clement.chadebec@orange.fr>
2021-12-22 11:19:19 +0100

    make quantizer

--- src/pythae/models/vq_vae/vq_vae_utils.py
+++ src/pythae/models/vq_vae/vq_vae_utils.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from ..base.base_utils import ModelOuput
+import torch.nn.functional as F
 
 from .vq_vae_config import VQVAEConfig
 
@@ -23,6 +24,8 @@ class Quantizer(nn.Module):
                 -1 / self.K, 1 / self.K)
 
     def forward(self, z: torch.Tensor):
+
+        z = z.permute(0, 2, 3, 1)        
         
         # TODO should be reshaped before enterring here []
 
@@ -32,4 +35,33 @@ class Quantizer(nn.Module):
         
         closest = distances.argmin(-1).unsqueeze(-1)
 
-        one_hot_encodings = nn.functionna
+        one_hot_encoding = F.one_hot(
+            cloesest, num_classes=self.num_embeddings
+        )
+
+        # quantization
+        quantized = one_hot_encoding @ self.embeddings.weight
+        quantized = quantized.reshape_as(z)
+
+        commitment_loss = F.mse_loss(
+            quantized.detach(),
+            z,
+            reduction='none'
+        ).mean(dim=-1)
+
+        embedding_loss =  F.mse_loss(
+            quantized,
+            z.detach(),
+            reduction='none'
+        ).mean(dim=-1)
+
+        quantized = z + (quantized - z).detach()
+
+        loss = commitment_loss * self.beta + embedding_loss
+
+        output = ModelOutput(
+            quantized_vector=quantized.permute(3, 1, 2),
+            loss=loss,
+            commitment_loss=commitment_loss,
+            embedding_loss=embedding_loss,
+        )
