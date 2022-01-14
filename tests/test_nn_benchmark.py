11a5471a4366ae9d57744d75d13b51896a053333
clementchadebec <clement.chadebec@orange.fr>
2022-01-14 13:34:42 +0100

    add default nn to tests

--- tests/test_nn_benchmark.py
+++ tests/test_nn_benchmark.py
@@ -6,6 +6,7 @@ from pythae.models import AEConfig, VAEConfig
 from pythae.models.nn.benchmarks.mnist import *
 from pythae.models.nn.benchmarks.celeba import *
 from pythae.models.nn.benchmarks.cifar import *
+from pythae.models.nn.default_architectures import *
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -21,20 +22,86 @@ class Test_MNIST_Benchmark:
 
     @pytest.fixture(
         params=[
-            [3, 4],
-            [np.random.randint(1, 5)],
-            [1, 2, 4],
+            [1],
             None
         ]
     )
-    def recon_layers(self, request):
+    def recon_layers_default(self, request):
         return request.param
 
     @pytest.fixture()
     def mnist_like_data(self):
         return torch.rand(3, 1, 28, 28).to(device)
 
-    def test_ae_encoding_decoding(self, ae_mnist_config, mnist_like_data, recon_layers):
+    def test_ae_encoding_decoding_default(
+        self, ae_mnist_config, mnist_like_data, recon_layers_default):
+        encoder = Encoder_AE_MLP(ae_mnist_config).to(device)
+        decoder = Decoder_AE_MLP(ae_mnist_config).to(device)
+
+        embedding = encoder(mnist_like_data).embedding
+    
+        assert embedding.shape == (mnist_like_data.shape[0], ae_mnist_config.latent_dim)
+
+        reconstruction = decoder(embedding).reconstruction
+
+        assert reconstruction.shape == mnist_like_data.shape
+
+        encoder_embed = encoder(mnist_like_data, output_layer_levels=recon_layers_default)
+        decoder_recon = decoder(embedding, output_layer_levels=recon_layers_default)
+
+        if recon_layers_default is None:
+            assert 'embedding' in encoder_embed.keys()
+
+        else:
+            for lev in recon_layers_default:
+                assert f'embedding_layer_{lev}' in encoder_embed.keys()
+
+        if recon_layers_default is None:
+            assert encoder_embed['embedding'].shape[1] == ae_mnist_config.latent_dim
+
+        else:
+            if 1 in recon_layers_default:
+                assert encoder_embed[f'embedding_layer_1'].shape[1] == 512
+                assert decoder_recon[f'reconstruction_layer_1'].shape[1] == 512
+
+    def test_discriminator_default(self, ae_mnist_config, mnist_like_data, recon_layers_default):
+        
+        ae_mnist_config.discriminator_input_dim = (1, 28, 28)
+
+        discriminator = Discriminator_MLP(ae_mnist_config).to(device)
+
+        scores = discriminator(mnist_like_data, output_layer_levels=recon_layers_default)
+
+        if recon_layers_default is None:
+            assert 'embedding' in scores.keys()
+
+        else:
+            for lev in recon_layers_default:
+                assert f'embedding_layer_{lev}' in scores.keys()
+
+        if recon_layers_default is None:
+            assert scores['embedding'].shape[1] == 1
+
+        else:
+            if 1 in recon_layers_default:
+                assert scores[f'embedding_layer_1'].shape[1] == 256
+
+            if 2 in recon_layers_default:
+                assert scores[f'embedding_layer_2'].shape[1] == 1
+
+    @pytest.fixture(
+    params=[
+            [3, 4],
+            [np.random.randint(1, 5)],
+            [1, 2, 4],
+            None
+        ]
+    )
+    def recon_layers_benchmark(self, request):
+        return request.param
+
+    def test_ae_encoding_decoding_benchmark(
+        self, ae_mnist_config, mnist_like_data, recon_layers_benchmark):
         encoder = Encoder_AE_MNIST(ae_mnist_config).to(device)
         decoder = Decoder_AE_MNIST(ae_mnist_config).to(device)
 
@@ -46,72 +113,73 @@ class Test_MNIST_Benchmark:
 
         assert reconstruction.shape == mnist_like_data.shape
 
-        encoder_embed = encoder(mnist_like_data, output_layer_levels=recon_layers)
-        decoder_recon = decoder(embedding, output_layer_levels=recon_layers)
+        encoder_embed = encoder(mnist_like_data, output_layer_levels=recon_layers_benchmark)
+        decoder_recon = decoder(embedding, output_layer_levels=recon_layers_benchmark)
 
-        if recon_layers is None:
+        if recon_layers_benchmark is None:
             assert 'embedding' in encoder_embed.keys()
 
         else:
-            for lev in recon_layers:
+            for lev in recon_layers_benchmark:
                 assert f'embedding_layer_{lev}' in encoder_embed.keys()
 
-        if recon_layers is None:
+        if recon_layers_benchmark is None:
             assert encoder_embed['embedding'].shape[1] == ae_mnist_config.latent_dim
 
         else:
-            if 1 in recon_layers:
+            if 1 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_1'].shape[1] == 128
                 assert decoder_recon[f'reconstruction_layer_1'].shape[1] == 1024
 
-            if 2 in recon_layers:
+            if 2 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_2'].shape[1] == 256
                 assert decoder_recon[f'reconstruction_layer_2'].shape[1] == 512
 
-            if 3 in recon_layers:
+            if 3 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_3'].shape[1] == 512
                 assert decoder_recon[f'reconstruction_layer_3'].shape[1] == 256
 
-            if 4 in recon_layers:
+            if 4 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_4'].shape[1] == 1024
                 assert decoder_recon[f'reconstruction_layer_4'].shape[1:] == ae_mnist_config.input_dim
 
-            if 5 in recon_layers:
+            if 5 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_5'].shape[1] == 1
 
 
-    def test_discriminator(self, ae_mnist_config, mnist_like_data, recon_layers):
+    def test_discriminator_benchmark(
+        self, ae_mnist_config, mnist_like_data, recon_layers_benchmark):
         
         ae_mnist_config.discriminator_input_dim = (1, 28, 28)
 
         discriminator = Discriminator_MNIST(ae_mnist_config).to(device)
 
-        scores = discriminator(mnist_like_data, output_layer_levels=recon_layers)
+        scores = discriminator(mnist_like_data, output_layer_levels=recon_layers_benchmark)
 
-        if recon_layers is None:
+        if recon_layers_benchmark is None:
             assert 'embedding' in scores.keys()
 
         else:
-            for lev in recon_layers:
+            for lev in recon_layers_benchmark:
                 assert f'embedding_layer_{lev}' in scores.keys()
 
-        if recon_layers is None:
+        if recon_layers_benchmark is None:
             assert scores['embedding'].shape[1] == 1
 
         else:
-            if 1 in recon_layers:
+            if 1 in recon_layers_benchmark:
                 assert scores[f'embedding_layer_1'].shape[1] == 128
 
-            if 2 in recon_layers:
+            if 2 in recon_layers_benchmark:
                 assert scores[f'embedding_layer_2'].shape[1] == 256
 
-            if 3 in recon_layers:
+            if 3 in recon_layers_benchmark:
                 assert scores[f'embedding_layer_3'].shape[1] == 512
 
-            if 4 in recon_layers:
+            if 4 in recon_layers_benchmark:
                 assert scores[f'embedding_layer_4'].shape[1] == 1024
 
-            if 5 in recon_layers:
+            if 5 in recon_layers_benchmark:
                 assert scores[f'embedding_layer_5'].shape[1] == 1
 
 
@@ -124,7 +192,7 @@ class Test_MNIST_Benchmark:
     def vae_mnist_config(self, request):
         return request.param
 
-    def test_vae_encoding_decoding(self, vae_mnist_config, mnist_like_data, recon_layers):
+    def test_vae_encoding_decoding(self, vae_mnist_config, mnist_like_data, recon_layers_benchmark):
         encoder = Encoder_VAE_MNIST(vae_mnist_config).to(device)
         decoder = Decoder_AE_MNIST(vae_mnist_config).to(device)
 
@@ -138,38 +206,39 @@ class Test_MNIST_Benchmark:
 
         assert reconstruction.shape == mnist_like_data.shape
 
-        encoder_embed = encoder(mnist_like_data, output_layer_levels=recon_layers)
-        decoder_recon = decoder(embedding, output_layer_levels=recon_layers)
+        encoder_embed = encoder(mnist_like_data, output_layer_levels=recon_layers_benchmark)
+        decoder_recon = decoder(embedding, output_layer_levels=recon_layers_benchmark)
 
-        if recon_layers is None:
+        if recon_layers_benchmark is None:
             assert 'embedding' in encoder_embed.keys()
 
         else:
-            for lev in recon_layers:
+            for lev in recon_layers_benchmark:
                 assert f'embedding_layer_{lev}' in encoder_embed.keys()
 
-        if recon_layers is None:
+        if recon_layers_benchmark is None:
             assert encoder_embed['embedding'].shape[1] == vae_mnist_config.latent_dim
 
         else:
-            if 1 in recon_layers:
+            if 1 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_1'].shape[1] == 128
                 assert decoder_recon[f'reconstruction_layer_1'].shape[1] == 1024
 
-            if 2 in recon_layers:
+            if 2 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_2'].shape[1] == 256
                 assert decoder_recon[f'reconstruction_layer_2'].shape[1] == 512
 
-            if 3 in recon_layers:
+            if 3 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_3'].shape[1] == 512
                 assert decoder_recon[f'reconstruction_layer_3'].shape[1] == 256
 
-            if 4 in recon_layers:
+            if 4 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_4'].shape[1] == 1024
                 assert decoder_recon[f'reconstruction_layer_4'].shape[1:] == vae_mnist_config.input_dim
 
-            if 5 in recon_layers:
+            if 5 in recon_layers_benchmark:
                 assert encoder_embed[f'embedding_layer_5'].shape[1] == 1
+
 
 class Test_CIFAR_Benchmark:
     @pytest.fixture(
