5d8a51b5476e83403e9e77adc6e8ee59dbf9a69b
clementchadebec <clement.chadebec@orange.fr>
2022-04-25 17:22:05 +0200

    add nll computation in IWAE

--- src/pythae/models/iwae/iwae_model.py
+++ src/pythae/models/iwae/iwae_model.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import torch
 import torch.nn.functional as F
+import numpy as np
 
 from ...data.datasets import BaseDataset
 from ..base.base_utils import ModelOutput
@@ -69,7 +70,7 @@ class IWAE(VAE):
         log_var = log_var.unsqueeze(1).repeat(1, self.n_samples, 1)
 
         std = torch.exp(0.5 * log_var)
-        z, eps = self._sample_gauss(mu, std)
+        z, _ = self._sample_gauss(mu, std)
         recon_x = self.decoder(z.reshape(-1, self.latent_dim))["reconstruction"]
 
         loss, recon_loss, kld = self.loss_function(recon_x, x, mu, log_var, z)
@@ -133,6 +134,88 @@ class IWAE(VAE):
         # Sample N(0, I)
         eps = torch.randn_like(std)
         return mu + eps * std, eps
+
+    def get_nll(self, data, n_samples=1, batch_size=100):
+        """
+        Function computed the estimate negative log-likelihood of the model. It uses importance
+        sampling method with the approximate posterior disctribution. This may take a while.
+
+        Args:
+            data (torch.Tensor): The input data from which the log-likelihood should be estimated.
+                Data must be of shape [Batch x n_channels x ...]
+            n_samples (int): The number of importance samples to use for estimation
+            batch_size (int): The batchsize to use to avoid memory issues
+        """
+
+        if n_samples <= batch_size:
+            n_full_batch = 1
+        else:
+            n_full_batch = n_samples // batch_size
+            n_samples = batch_size
+
+        log_p = []
+
+        for i in range(len(data)):
+            x = data[i].unsqueeze(0)
+
+            log_p_x = []
+
+            for j in range(n_full_batch):
+
+                x_rep = torch.cat(batch_size * [x])
+
+                encoder_output = self.encoder(x_rep)
+                mu, log_var = encoder_output.embedding, encoder_output.log_covariance
+
+                mu = mu.unsqueeze(1).repeat(1, self.n_samples, 1)
+                log_var = log_var.unsqueeze(1).repeat(1, self.n_samples, 1)
+
+                std = torch.exp(0.5 * log_var)
+                z, _ = self._sample_gauss(mu, std)
+
+                log_q_z_given_x = -0.5 * (
+                    log_var + (z - mu) ** 2 / torch.exp(log_var)
+                ).sum(dim=-1)
+                log_p_z = -0.5 * (z**2).sum(dim=-1)
+
+                recon_x = self.decoder(z.reshape(-1, self.latent_dim))["reconstruction"]
+                
+                if self.model_config.reconstruction_loss == "mse":
+
+                    log_p_x_given_z = -0.5 * F.mse_loss(
+                        recon_x.reshape(recon_x.shape[0], -1),
+                        x_rep.reshape(x_rep.shape[0], -1)
+                        .unsqueeze(1)
+                        .repeat(1, self.n_samples, 1)
+                        .reshape(recon_x.shape[0], -1),
+                        reduction="none",
+                    ).sum(dim=-1).reshape(x_rep.shape[0], -1) - torch.tensor(
+                        [np.prod(self.input_dim) / 2 * np.log(np.pi * 2)]
+                    ).to(
+                        data.device
+                    )  # decoding distribution is assumed unit variance  N(mu, I)
+
+                elif self.model_config.reconstruction_loss == "bce":
+
+                    log_p_x_given_z = -F.binary_cross_entropy(
+                        recon_x.reshape(recon_x.shape[0], -1),
+                        x_rep.reshape(x_rep.shape[0], -1)
+                        .unsqueeze(1)
+                        .repeat(1, self.n_samples, 1)
+                        .reshape(recon_x.shape[0], -1),
+                        reduction="none",
+                    ).sum(dim=-1).reshape(x_rep.shape[0], -1)
+
+                log_w = log_p_x_given_z + log_p_z - log_q_z_given_x
+
+                log_p_x.append(
+                    (log_w).exp().mean(dim=-1).log()
+                )  # log(2*pi) simplifies
+
+            log_p_x = torch.cat(log_p_x)
+
+            log_p.append((torch.logsumexp(log_p_x, 0) - np.log(len(log_p_x))).item())
+        return np.mean(log_p)
 
     @classmethod
     def _load_model_config_from_folder(cls, dir_path):
