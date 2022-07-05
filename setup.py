acbd7e087f4f3847bc4f3e112f585b7b07c84019
Sugato Ray <sugatoray@users.noreply.github.com>
2022-07-04 22:32:56 -0500

    update setup.py: comment unused code

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import pathlib
 import pkg_resources
 from setuptools import find_packages, setup
 
-with pathlib.Path("requirements.txt").open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement in pkg_resources.parse_requirements(requirements_txt)
-    ]
+# with pathlib.Path("requirements.txt").open() as requirements_txt:
+#     install_requires = [
+#         str(requirement)
+#         for requirement in pkg_resources.parse_requirements(requirements_txt)
+#     ]
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
