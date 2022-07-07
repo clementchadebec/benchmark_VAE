1403796d8f2f002b7eba244f8e186971aa7d7f38
clementchadebec <clement.chadebec@orange.fr>
2022-07-07 19:45:36 +0200

    fix missing model_card

--- src/pythae/models/base/base_utils.py
+++ src/pythae/models/base/base_utils.py
@@ -12,6 +12,21 @@ console = logging.StreamHandler()
 logger.addHandler(console)
 logger.setLevel(logging.INFO)
 
+model_card_template = """---
+language: en
+tags:
+- pythae
+license: apache-2.0
+---
+
+### Downloading this model from the Hub
+This model was trained with pythae. It can be downloaded or reloaded using the method `load_from_hf_hub`
+```python
+>>> from pythae.models import AutoModel
+>>> model = AutoModel.load_from_hf_hub(hf_hub_path="your_hf_username/repo_name")
+```
+"""
+
 
 def hf_hub_is_available():
     return importlib.util.find_spec("huggingface_hub") is not None
