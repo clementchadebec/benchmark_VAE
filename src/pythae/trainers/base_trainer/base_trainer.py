19c19a4ac8d295af1fab94f1954ebf224b537adf
clementchadebec <clement.chadebec@orange.fr>
2023-02-03 14:46:31 +0100

    add barrier

--- src/pythae/trainers/base_trainer/base_trainer.py
+++ src/pythae/trainers/base_trainer/base_trainer.py
@@ -379,7 +379,7 @@ class BaseTrainer:
         Args:
             log_output_dir (str): The path in which the log will be stored
         """
-
+        dist.barrier()
         self.prepare_training()
 
         self.callback_handler.on_train_begin(
