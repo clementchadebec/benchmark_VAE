ef6f02deaa32cb8401497c44c4b3503492f160fa
clementchadebec <clement.chadebec@orange.fr>
2023-02-04 18:49:33 +0100

    add sigmoid

--- examples/scripts/distributed_training_ffhq.py
+++ examples/scripts/distributed_training_ffhq.py
@@ -101,7 +101,8 @@ class Decoder_ResNet_VQVAE_FFHQ(BaseDecoder):
                 nn.ConvTranspose2d(256, 128, 4, 2, padding=1),
                 nn.ConvTranspose2d(128, 64, 4, 2, padding=1),
                 nn.ConvTranspose2d(64, 32, 4, 2, padding=1),
-                nn.ConvTranspose2d(32, self.n_channels, 4, 2, padding=1)
+                nn.ConvTranspose2d(32, self.n_channels, 4, 2, padding=1),
+                nn.Sigmoid()
             )
 
 
