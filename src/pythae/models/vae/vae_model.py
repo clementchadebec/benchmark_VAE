e0d9d13e0fd319617818f4980e34187ecaacacc0
clementchadebec <clement.chadebec@orange.fr>
2023-04-11 12:37:42 +0200

    remove print

--- src/pythae/models/vae/vae_model.py
+++ src/pythae/models/vae/vae_model.py
@@ -100,7 +100,6 @@ class VAE(BaseAE):
     def loss_function(self, recon_x, x, mu, log_var, z):
 
         if self.model_config.reconstruction_loss == "mse":
-            print('here')
             recon_loss = 0.5 * F.mse_loss(
                 recon_x.reshape(x.shape[0], -1),
                 x.reshape(x.shape[0], -1),
