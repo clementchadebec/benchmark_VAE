e749a542d068ea6460c710fc2c70590c97cc81b1
Clément Chadebec <47564971+clementchadebec@users.noreply.github.com>
2024-07-17 09:34:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pythae",
-    version="0.1.2",
+    version="0.1.3",
     author="Clement Chadebec",
     author_email="clement.chadebec@gmail.com",
     description="Unifying Generative Autoencoders in Python",
